1e644525797a3f5ecbdafa6f349a67c827d617a6
Christian Duvholt <christian@duvholt.net>
2020-02-21 14:08:09 +0100

    Autostart statusnotifierwatcher for tray icons

--- local/bin/i3_kde_services.sh
+++ local/bin/i3_kde_services.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-for service in accounts touchpad ksysguard plasmavault bluedevil freespacenotifier kscreen networkmanagement keyboard; do qdbus org.kde.kded5 /kded org.kde.kded5.loadModule $service; done
+for service in accounts touchpad ksysguard plasmavault bluedevil freespacenotifier kscreen networkmanagement keyboard statusnotifierwatcher; do qdbus org.kde.kded5 /kded org.kde.kded5.loadModule $service; done
