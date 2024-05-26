9c65570a9d2950d39814ab5722db9235a9a8cd1c
Christian Duvholt <christian.duvholt@nrk.no>
2024-05-26 19:58:32 +0200

    Cleanup i3 config

--- local/bin/i3_kde_services.sh
+++ local/bin/i3_kde_services.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-for service in accounts touchpad ksysguard plasmavault bluedevil freespacenotifier kscreen networkmanagement keyboard statusnotifierwatcher; do qdbus org.kde.kded5 /kded org.kde.kded5.loadModule $service; done
+#for service in accounts touchpad ksysguard plasmavault bluedevil freespacenotifier kscreen networkmanagement keyboard statusnotifierwatcher; do qdbus org.kde.kded5 /kded org.kde.kded5.loadModule $service; done
+for service in accounts touchpad ksysguard plasmavault bluedevil freespacenotifier kscreen networkmanagement keyboard statusnotifierwatcher; do qdbus org.kde.kded6 /kded org.kde.kded6.loadModule $service; done
