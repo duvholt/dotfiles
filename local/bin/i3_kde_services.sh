e1b4d28947d913a46d4a584d5e41c28ddf924c5e
Christian Duvholt <christian.duvholt@nrk.no>
2024-06-09 15:18:36 +0200

    i3 + KDE tweaks

--- local/bin/i3_kde_services.sh
+++ local/bin/i3_kde_services.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-#for service in accounts touchpad ksysguard plasmavault bluedevil freespacenotifier kscreen networkmanagement keyboard statusnotifierwatcher; do qdbus org.kde.kded5 /kded org.kde.kded5.loadModule $service; done
-for service in accounts touchpad ksysguard plasmavault bluedevil freespacenotifier kscreen networkmanagement keyboard statusnotifierwatcher; do qdbus org.kde.kded6 /kded org.kde.kded6.loadModule $service; done
+for service in accounts touchpad ksysguard plasmavault bluedevil freespacenotifier kscreen networkmanagement keyboard statusnotifierwatcher mprisservice plasma-session-shortcuts gtkconfig inotify kded_bolt ktimezoned kded_touchpad; do qdbus org.kde.kded6 /kded org.kde.kded6.loadModule $service; done
