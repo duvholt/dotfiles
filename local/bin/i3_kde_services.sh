078c15a428d7cf9a0e0a5ac92470af12efdc1281
Christian Duvholt <christian.duvholt@nrk.no>
2025-08-03 13:00:59 +0200

    Start kde services using qdbus6

--- local/bin/i3_kde_services.sh
+++ local/bin/i3_kde_services.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-for service in accounts touchpad ksysguard plasmavault bluedevil freespacenotifier kscreen networkmanagement keyboard statusnotifierwatcher mprisservice plasma-session-shortcuts gtkconfig inotify kded_bolt ktimezoned kded_touchpad; do qdbus org.kde.kded6 /kded org.kde.kded6.loadModule $service; done
+for service in accounts touchpad ksysguard plasmavault bluedevil freespacenotifier kscreen networkmanagement keyboard statusnotifierwatcher mprisservice plasma-session-shortcuts gtkconfig inotify kded_bolt ktimezoned kded_touchpad; do qdbus6 org.kde.kded6 /kded org.kde.kded6.loadModule $service; done
